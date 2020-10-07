b8af1de99bb2be54b57f6e13257e5d7db58348ab
hushun1214 <2375353463@qq.com>
2020-10-07 11:25:08 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,15 +1,6 @@
 <template>
-  <div
-    class="col"
-    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-    :style="{
-      paddingLeft: gutter / 2 + 'px',
-      paddingRight: gutter / 2 + 'px',
-    }"
-  >
-    <div style="border: 1px solid green; height: 50px;">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -28,6 +19,18 @@ export default {
     return {
       gutter: 0,
     };
+  },
+  computed: {
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + "px",
+        paddingRight: this.gutter / 2 + "px",
+      };
+    },
+    colClass() {
+      let { span, offset } = this;
+      return [span && `col-${span}`, offset && `offset-${offset}`];
+    },
   },
 };
 </script>
