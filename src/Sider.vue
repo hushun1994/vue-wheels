eb9ce0b502498fcb33f9e0bd8fb96e243fc72fd4
hushun1214 <2375353463@qq.com>
2020-10-07 21:52:07 +0800

    添加简单动画

--- src/Sider.vue
+++ src/Sider.vue
@@ -1,13 +1,47 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="hide">close</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: "wheelsSider",
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  methods: {
+    hide() {
+      this.visible = !this.visible;
+    },
+    enter(el, done) {
+      el.marginLeft = "-200px";
+      done();
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.7s;
+}
+.slide-enter,
+.slide-leave-to {
+  margin-left: -200px;
+}
+</style>
