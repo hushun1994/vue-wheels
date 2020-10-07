c26c063b725060493961e980003f30c7df22f84d
hushun1214 <2375353463@qq.com>
2020-10-07 21:29:49 +0800

    Layout 基本布局功能完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from "./Button-group";
 import Input from "./Input";
 import Row from "./Row";
 import Col from "./Col";
+import Layout from "./Layout";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import Sider from "./Sider";
 
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
@@ -12,6 +17,11 @@ Vue.component("g-button-group", ButtonGroup);
 Vue.component("g-input", Input);
 Vue.component("g-row", Row);
 Vue.component("g-col", Col);
+Vue.component("g-layout", Layout);
+Vue.component("g-header", Header);
+Vue.component("g-content", Content);
+Vue.component("g-footer", Footer);
+Vue.component("g-sider", Sider);
 
 new Vue({
   el: "#app",
