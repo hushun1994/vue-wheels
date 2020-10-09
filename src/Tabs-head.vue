2c6ba4c14154440d7a9fcc82d275705a3fadf5a0
hushun1214 <2375353463@qq.com>
2020-10-09 16:22:17 +0800

    完善 tabs 的样式

--- src/Tabs-head.vue
+++ src/Tabs-head.vue
@@ -27,20 +27,26 @@ export default {
 <style lang="scss" scoped>
 $tab-height: 40px;
 $blue: blue;
+$border-color: #ddd;
 .tabs-head {
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
-  align-items: center;
+  /* align-items: center; */
   position: relative;
+  border-bottom: 1px solid $border-color;
   > .line {
     position: absolute;
     bottom: 0;
-    border-bottom: 2px solid $blue;
+    border-bottom: 1px solid $blue;
     transition: all 0.25s;
   }
   > .actions-wrapper {
     margin-left: auto;
+    padding: 0 1em;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
   }
 }
 </style>
