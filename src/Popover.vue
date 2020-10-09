7cd7b94352227cb80c86e9ed9f25421842f45bd9
hushun1214 <2375353463@qq.com>
2020-10-09 22:13:06 +0800

    实现最简单的 popover

---
+++ src/Popover.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" @click.stop>
+      <slot name="content"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "wheelsPopover",
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    xxx() {
+      this.visible = !this.visible;
+      if (this.visible === true) {
+        // this.$nextTick(() => {
+        //   document.body.addEventListener("click", () => {
+        //     this.visible = false;
+        //   });
+        // });
+        setTimeout(() => {
+          let eventHandler = () => {
+            this.visible = false;
+            document.removeEventListener("click", eventHandler);
+          };
+          document.addEventListener("click", eventHandler);
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+  > .content-wrapper {
+    position: absolute;
+    bottom: 100%;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ import TabsHead from "./Tabs-head";
 import TabsBody from "./Tabs-body";
 import TabsItem from "./Tabs-item";
 import TabsPane from "./Tabs-pane";
+import Popover from "./Popover";
 
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
@@ -35,6 +36,7 @@ Vue.component("g-tabs-head", TabsHead);
 Vue.component("g-tabs-body", TabsBody);
 Vue.component("g-tabs-item", TabsItem);
 Vue.component("g-tabs-pane", TabsPane);
+Vue.component("g-popover", Popover);
 
 Vue.use(plugin);
 
