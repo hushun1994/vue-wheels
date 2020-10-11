22a99e5a291ffa2ba9a7c45301cb0f3b4e3371f7
hushun1214 <2375353463@qq.com>
2020-10-11 22:21:30 +0800

    重构代码

--- src/Tabs.vue
+++ src/Tabs.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Vue from "vue";
+import Vue from "vue"
 export default {
   name: "wheelsTabs",
   props: {
@@ -17,42 +17,50 @@ export default {
       type: String,
       default: "horizontal",
       validator(value) {
-        return ["horizontal", "vertical"].indexOf(value) >= 0;
+        return ["horizontal", "vertical"].indexOf(value) >= 0
       },
     },
   },
   data() {
     return {
       eventBus: new Vue(),
-    };
+    }
   },
   provide() {
     return {
       eventBus: this.eventBus,
-    };
+    }
+  },
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console &&
+          console.warn &&
+          console.warn(
+            "tabs 的子组件应该是 tabs-head 和 tabs-body，但你没有写子组件"
+          )
+      }
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === "wheelsTabsHead") {
+          vm.$children.forEach((childVm) => {
+            if (
+              childVm.$options.name === "wheelsTabsItem" &&
+              childVm.name === this.selected
+            ) {
+              this.eventBus.$emit("update:selected", this.selected, childVm)
+            }
+          })
+        }
+      })
+    },
   },
   mounted() {
-    if (this.$children.length === 0) {
-      console &&
-        console.warn &&
-        console.warn(
-          "tabs 的子组件应该是 tabs-head 和 tabs-body，但你没有写子组件"
-        );
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === "wheelsTabsHead") {
-        vm.$children.forEach((childVm) => {
-          if (
-            childVm.$options.name === "wheelsTabsItem" &&
-            childVm.name === this.selected
-          ) {
-            this.eventBus.$emit("update:selected", this.selected, childVm);
-          }
-        });
-      }
-    });
+    this.checkChildren()
+    this.selectTab()
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
