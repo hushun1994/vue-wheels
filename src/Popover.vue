c36cb7335bce4ea99e0d9fe70535b6b19fb5668a
hushun1214 <2375353463@qq.com>
2020-10-11 22:33:36 +0800

    完成 popover 基本功能

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click.stop="xxx">
+  <div class="popover" @click="onClick" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -15,31 +15,52 @@ export default {
   data() {
     return {
       visible: false,
-    };
+    }
   },
   methods: {
-    xxx() {
-      this.visible = !this.visible;
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper);
-          let { left, top } = this.$refs.triggerWrapper.getBoundingClientRect();
-          this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`;
-          this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`;
-          // let { offsetLeft, offsetTop } = this.$refs.triggerWrapper;
-          // this.$refs.contentWrapper.style.top = `${this.$el.offsetTop}px`;
-          // this.$refs.contentWrapper.style.left = `${this.$el.offsetLeft}px`;
-          let eventHandler = () => {
-            this.visible = false;
-            document.removeEventListener("click", eventHandler);
-          };
-          document.body.addEventListener("click", eventHandler);
-        });
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper)
+      let { left, top } = this.$refs.triggerWrapper.getBoundingClientRect()
+      this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`
+      this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`
+    },
+    onClickDocument(e) {
+      if (
+        (this.$refs.popover && this.$refs.popover.contains(e.target)) ||
+        (this.$refs.contentWrapper &&
+          this.$refs.contentWrapper.contains(e.target))
+      ) {
+        return
+      }
+      this.close()
+    },
+    open() {
+      this.visible = true
+      this.$nextTick(() => {
+        this.positionContent()
+        document.addEventListener("click", this.onClickDocument)
+      })
+    },
+    close() {
+      this.visible = false
+      document.removeEventListener("click", this.onClickDocument)
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close()
+        } else {
+          this.open()
+        }
       }
     },
   },
-  mounted() {},
-};
+  mounted() {
+    // document.addEventListener("click", () => {
+    //   console.log("最初的绑定")
+    // })
+  },
+}
 </script>
 
 <style lang="scss" scoped>
