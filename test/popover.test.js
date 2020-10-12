4cf8b68381b5dfda6fd3d9c0ceb353bf6564465e
hushun1214 <2375353463@qq.com>
2020-10-13 00:42:29 +0800

    有一个测试用例不知道如何实现，先跳过

--- test/popover.test.js
+++ test/popover.test.js
@@ -30,10 +30,36 @@ describe('Popover', () => {
       const { contentWrapper } = vm.$refs.a.$refs
       expect(contentWrapper.classList.contains('position-bottom')).to.be.true
       done()
+      div.remove()
+      vm.$destroy()
     })
-    // const element = vm.$el
-    // expect(getComputedStyle(element).justifyContent).to.eq('flex-end')
-    // div.remove()
-    // vm.$destroy()
+  })
+
+  xit('接收 trigger 属性', (done) => {
+    Vue.component('g-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <g-popover position='bottom' ref="a">
+        <template slot="content">
+          <div>popover内容</div>
+        </template>
+        <button>点击</button>
+      </g-popover>
+    `
+    const vm = new Vue({
+      el: div,
+    })
+    let event = new Event('mouseenter')
+    vm.$el.dispatchEvent(event)
+    vm.$nextTick(() => {
+      const { contentWrapper } = vm.$refs.a.$refs
+      console.log(contentWrapper)
+      console.log(vm.$refs.a.visible)
+      expect(contentWrapper).to.exist
+      done()
+      div.remove()
+      vm.$destroy()
+    })
   })
 })
