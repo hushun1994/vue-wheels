995a5b85da43481573d9160025f930f8883679a3
hushun1214 <2375353463@qq.com>
2020-10-13 13:36:25 +0800

    重构代码

--- src/Collapse.vue
+++ src/Collapse.vue
@@ -14,7 +14,7 @@ export default {
       default: false,
     },
     selected: {
-      type: String,
+      type: Array,
     },
   },
   data() {
@@ -23,17 +23,29 @@ export default {
     }
   },
   provide() {
-    // if (this.single) {
-    //   return {
-    //     eventBus: this.eventBus,
-    //   }
-    // }
     return {
       eventBus: this.eventBus,
     }
   },
   mounted() {
     this.eventBus.$emit('update:selected', this.selected)
+    this.eventBus.$on('update:addSelected', (name) => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+      if (this.single) {
+        selectedCopy = [name]
+      } else {
+        selectedCopy.push(name)
+      }
+      this.$emit('update:selected', selectedCopy)
+      this.eventBus.$emit('update:selected', selectedCopy)
+    })
+    this.eventBus.$on('update:removeSelected', (name) => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+      let index = selectedCopy.indexOf(name)
+      selectedCopy.splice(index, 1)
+      this.$emit('update:selected', selectedCopy)
+      this.eventBus.$emit('update:selected', selectedCopy)
+    })
   },
 }
 </script>
