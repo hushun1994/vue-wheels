7b41dc6ed054c7cfd7ce5cf6c54f195eeb1eebb1
hushun1214 <2375353463@qq.com>
2020-10-17 09:34:39 +0800

    添加 Tabs 切换动画

--- src/Tabs-pane.vue
+++ src/Tabs-pane.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: "wheelsTabsPane",
+  name: 'wheelsTabsPane',
   props: {
     name: {
       type: String | Number,
@@ -16,28 +16,46 @@ export default {
   data() {
     return {
       active: false,
-    };
+    }
   },
   computed: {
     classes() {
       return {
         active: this.active,
-      };
+      }
     },
   },
-  inject: ["eventBus"],
+  inject: ['eventBus'],
   created() {
-    this.eventBus.$on("update:selected", (name) => {
-      this.active = name === this.name;
-    });
+    this.eventBus.$on('update:selected', (name) => {
+      this.active = name === this.name
+    })
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
+@keyframes slidein {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
+}
+@keyframes slideout {
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
 .tabs-pane {
   padding: 1em;
+  animation: slideout 0.25s linear;
   &.active {
+    animation: slidein 0.25s linear;
   }
 }
 </style>
